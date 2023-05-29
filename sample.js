ce7b5c10312841df1fbbc0333333aab3ade8c11f
tomohiro1024 <gongyezhihong732@gmail.com>
2023-05-29 21:38:03 +0900

    条件分岐

--- sample.js
+++ sample.js
@@ -4,14 +4,24 @@ const fs = require('fs')
 
 console.log('start')
 
-// fs.writeFileSync('test.txt', 'Hello', function() {
-//     console.log('OK')
-// })
 
-// fs.readFile('./test.txt', 'utf8', function(err, data){
-//    console.log(data)
-// })
+
+
 
 console.log(process.argv[2])
+
+const request = process.argv[2]
+
+if(request === 'read') {
+    fs.readFile('./test.txt', 'utf8', function(err, data){
+        console.log(data)
+     })
+} else if (request === 'write') {
+    fs.writeFileSync('test.txt', 'Hello', function() {
+        console.log('OK')
+    })
+} else {
+    console.error('error!!!')
+}
 
 console.log('end')
