4e0de29f221c1290afd4e592b8bd010bcfb93c51
tomohiro1024 <gongyezhihong732@gmail.com>
2023-05-29 18:46:09 +0900

    process.argv

--- sample.js
+++ sample.js
@@ -2,11 +2,16 @@
 
 const fs = require('fs')
 
+console.log('start')
 
 // fs.writeFileSync('test.txt', 'Hello', function() {
 //     console.log('OK')
 // })
 
-fs.readFile('./test.txt', 'utf8', function(err, data){
-   console.log(data)
-})
+// fs.readFile('./test.txt', 'utf8', function(err, data){
+//    console.log(data)
+// })
+
+console.log(process.argv[2])
+
+console.log('end')
