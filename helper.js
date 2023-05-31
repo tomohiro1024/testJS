37042c95925bbca4ba0dd2e103d14ed02bd12f93
tomohiro1024 <gongyezhihong732@gmail.com>
2023-05-31 12:08:21 +0900

    JSON

--- helper.js
+++ helper.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 
+const person = {name: 'Tanaka', from: 'Tokyo', sports: 'soccer', }
+
 const read = function () {
     fs.readFile('./test.txt', 'utf8', function(err, data){
         console.log(data)
@@ -7,7 +9,7 @@ const read = function () {
 }
 
 const write = function () {
-    fs.writeFileSync('test.txt', 'Hello!!!', function() {
+    fs.writeFileSync('test.json', JSON.stringify(person), function() {
         console.log('OK')
     })
 } 
