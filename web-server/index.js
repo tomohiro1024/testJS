0e655dd7cd66c6e533970936ff770c6908f7a23b
tomohiro1024 <gongyezhihong732@gmail.com>
2023-06-04 11:21:56 +0900

    htmlファイルの読み込み

--- web-server/index.js
+++ web-server/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/', function (req, res) {
-    console.log(req)
-  res.send('<h1>Hello World!</h1>')
-})
+app.use(express.static(path.join(__dirname,'public')))
+
+// app.get('/', function (req, res) {
+//     console.log(req)
+//   res.send('<h1>Hello World!</h1>')
+// })
 
 app.get('/apple', function (req, res) {
     res.send('APPLE!!!!!!')
