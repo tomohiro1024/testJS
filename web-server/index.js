d55be047f505b54fadbb99d1c5d0068f4d0ab010
tomohiro1024 <gongyezhihong732@gmail.com>
2023-06-13 11:30:21 +0900

    送信

--- web-server/index.js
+++ web-server/index.js
@@ -2,12 +2,19 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.urlencoded({ extended:false }))
+
 app.use(express.static(path.join(__dirname,'public')))
 
 // app.get('/', function (req, res) {
 //     console.log(req)
 //   res.send('<h1>Hello World!</h1>')
 // })
+
+app.post('/api/v1/quiz', function (req, res) {
+  const answer = req.body.answer
+  res.send(answer)
+})
 
 app.get('/apple', function (req, res) {
     res.send('APPLE!!!!!!')
